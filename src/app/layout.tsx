9cfe38bb56207a11211dd0dd4c487124f90a16e9
DustineConsina <dustineconsina2@gmail.com>
2025-04-27 23:06:31 +0800

    frontend files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import Navbar from './components/Navbar';
+import { Toaster } from "react-hot-toast"; 
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,8 +16,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <h1> tung tung tung sahurr</h1>
-       
+        <Toaster />  
+        <Navbar />
+        {children}
       </body>
     </html>
   );
